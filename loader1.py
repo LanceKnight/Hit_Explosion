95c30c0497e9939a164fd68a83491f6b986a7034
Yunchao "Lance" Liu <lanceknight26@gmail.com>
2021-06-05 09:03:26 -0500

    Create loader1.py

---
+++ loader1.py
@@ -0,0 +1,199 @@
+import os
+import torch
+import pickle
+import collections
+import math
+import pandas as pd
+import numpy as np
+import networkx as nx
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
+from rdkit.Chem.rdMolDescriptors import GetMorganFingerprintAsBitVect
+from torch.utils import data
+from torch_geometric.data import Data
+from torch_geometric.data import InMemoryDataset
+from torch_geometric.data import Batch
+from itertools import repeat, product, chain
+
+
+# allowable node and edge features
+allowable_features = {
+    'possible_atomic_num_list': list(range(1, 119)),
+    'possible_formal_charge_list': [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5],
+    'possible_chirality_list': [
+        Chem.rdchem.ChiralType.CHI_UNSPECIFIED,
+        Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW,
+        Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW,
+        Chem.rdchem.ChiralType.CHI_OTHER
+    ],
+    'possible_hybridization_list': [
+        Chem.rdchem.HybridizationType.S,
+        Chem.rdchem.HybridizationType.SP, Chem.rdchem.HybridizationType.SP2,
+        Chem.rdchem.HybridizationType.SP3, Chem.rdchem.HybridizationType.SP3D,
+        Chem.rdchem.HybridizationType.SP3D2, Chem.rdchem.HybridizationType.UNSPECIFIED
+    ],
+    'possible_numH_list': [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    'possible_implicit_valence_list': [0, 1, 2, 3, 4, 5, 6],
+    'possible_degree_list': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    'possible_bonds': [
+        Chem.rdchem.BondType.SINGLE,
+        Chem.rdchem.BondType.DOUBLE,
+        Chem.rdchem.BondType.TRIPLE,
+        Chem.rdchem.BondType.AROMATIC
+    ],
+    'possible_bond_dirs': [  # only for double bond stereo information
+        Chem.rdchem.BondDir.NONE,
+        Chem.rdchem.BondDir.ENDUPRIGHT,
+        Chem.rdchem.BondDir.ENDDOWNRIGHT
+    ]
+}
+
+
+def mol_to_graph_data_obj_simple(mol):
+    """
+    Converts rdkit mol object to graph Data object required by the pytorch
+    geometric package. NB: Uses simplified atom and bond features, and represent
+    as indices
+    :param mol: rdkit mol object
+    :return: graph data object with the attributes: x, edge_index, edge_attr
+    """
+    # atoms
+    num_atom_features = 2   # atom type,  chirality tag
+    atom_features_list = []
+    for atom in mol.GetAtoms():
+        atom_feature = [allowable_features['possible_atomic_num_list'].index(
+            atom.GetAtomicNum())] + [allowable_features[
+                'possible_chirality_list'].index(atom.GetChiralTag())]
+        atom_features_list.append(atom_feature)
+    x = torch.tensor(np.array(atom_features_list), dtype=torch.long)
+
+    # bonds
+    num_bond_features = 2   # bond type, bond direction
+    if len(mol.GetBonds()) > 0:  # mol has bonds
+        edges_list = []
+        edge_features_list = []
+        for bond in mol.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+            edge_feature = [allowable_features['possible_bonds'].index(
+                bond.GetBondType())] + [allowable_features[
+                    'possible_bond_dirs'].index(
+                    bond.GetBondDir())]
+            edges_list.append((i, j))
+            edge_features_list.append(edge_feature)
+            edges_list.append((j, i))
+            edge_features_list.append(edge_feature)
+
+        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
+        edge_index = torch.tensor(np.array(edges_list).T, dtype=torch.long)
+
+        # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
+        edge_attr = torch.tensor(np.array(edge_features_list),
+                                 dtype=torch.long)
+    else:   # mol has no bonds
+        edge_index = torch.empty((2, 0), dtype=torch.long)
+        edge_attr = torch.empty((0, num_bond_features), dtype=torch.long)
+
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+
+    return data
+
+
+class MoleculeDataset(InMemoryDataset):
+    def __init__(self,
+                 root,
+                 #data = None,
+                 #slices = None,
+                 transform=None,
+                 pre_transform=None,
+                 pre_filter=None,
+                 dataset='435008',
+                 empty=False):
+        """
+        Adapted from qm9.py. Disabled the download functionality
+        :param root: directory of the dataset, containing a raw and processed
+        dir. The raw dir should contain the file containing the smiles, and the
+        processed dir can either empty or a previously processed file
+        :param dataset: name of the dataset. Currently only implemented for
+        zinc250k, chembl_with_labels, tox21, hiv, bace, bbbp, clintox, esol,
+        freesolv, lipophilicity, muv, pcba, sider, toxcast
+        :param empty: if True, then will not load any data obj. For
+        initializing empty dataset
+        """
+        self.dataset = dataset
+        self.root = root
+
+        super(MoleculeDataset, self).__init__(root, transform, pre_transform,
+                                              pre_filter)
+        self.transform, self.pre_transform, self.pre_filter = transform, pre_transform, pre_filter
+
+        if not empty:
+            self.data, self.slices = torch.load(self.processed_paths[0])
+
+    # def get(self, idx):
+    #     data = Data()
+    #     print(f'data.keys:{self.data.keys}')
+    #     for key in self.data.keys:
+    #         item, slices = self.data[key], self.slices[key]
+    #         print(f'item:{len(item)}, slices:{len(slices)}')
+    #         s = list(repeat(slice(None), item.dim()))
+    #         s[data.__cat_dim__(key, item)] = slice(slices[idx],
+    #                                                slices[idx + 1])
+    #         data[key] = item[s]
+    #     return data
+
+    @property
+    def raw_file_names(self):
+        file_name_list = os.listdir(self.raw_dir)
+        # assert len(file_name_list) == 1     # currently assume we have a
+        # # single raw file
+        return file_name_list
+
+    @property
+    def processed_file_names(self):
+        return 'geometric_data_processed.pt'
+
+    def download(self):
+        raise NotImplementedError('Must indicate valid location of raw data. '
+                                  'No download allowed')
+
+    def process(self):
+        print('processing123')
+        data_list = []
+        print(f"path:{self.raw_paths[0]}")
+        if self.dataset == '435008':
+            try:
+                suppl = Chem.SDMolSupplier(self.raw_paths[0])
+                print(f'len:{len(suppl)}')
+                for i, mol in enumerate(suppl):
+                    if mol != None:
+                        smiles = Chem.MolToSmiles(mol)
+                        print(f"i:{i}, {smiles}")
+                        data = mol_to_graph_data_obj_simple(mol)
+                        # manually add mol id
+                        data.id = torch.tensor([i])
+                        data.smiles = smiles
+                        print(type(data))
+                        data_list.append(data)
+                    else:
+                        print('mol is None')
+            except Exception as e:
+                print('connot convert .sdf file:' + e)
+
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
+
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        # write data_smiles_list in processed paths
+
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
+
+if __name__ == "__main__":
+    a = MoleculeDataset(root="data")
+    t = a[0:3]
